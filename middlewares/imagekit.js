d5b2fdad4dd314ee216979d56bc4e6bd1894e1a7
Rizqiiiii <rizqyadityaaa129@gmail.com>
2022-06-18 13:44:31 +0700

    test

--- middlewares/imagekit.js
+++ middlewares/imagekit.js
@@ -1,6 +1,6 @@
 const FormData = require("form-data");
 const axios = require("axios");
-const IMGKIT_API = process.env.IMAGEKIT_PRIVATEKEY;
+const IMGKIT_API = "private_a3hZtYATBTb0ligtNkOVj5Tog4A="
 
 function imgKit(req, res, next) {
   if (req.file) {
